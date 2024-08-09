360f7e5c4c678561f6c819a4ba9c135abd9beb17
David Hérault <dherault@gmail.com>
2024-08-09 19:29:24 +0300

    Create language-keywords

---
+++ packages/language-keywords/src/index.ts
@@ -0,0 +1,15 @@
+export { default as javascript } from './javascript'
+
+export { default as typescript } from './typescript'
+
+export { default as python } from './python'
+
+export { default as c } from './c'
+
+export { default as cplusplus } from './cplusplus'
+
+export { default as csharp } from './csharp'
+
+export { default as ruby } from './ruby'
+
+export { default as php } from './php'
