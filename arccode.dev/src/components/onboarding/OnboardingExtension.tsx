76431aae4e82f7bc25115f4251506dfc485a85cb
David Hérault <dherault@gmail.com>
2024-08-09 18:45:52 +0300

    Enhance OnboardingExtension

--- arccode.dev/src/components/onboarding/OnboardingExtension.tsx
+++ arccode.dev/src/components/onboarding/OnboardingExtension.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import useUser from '~hooks/user/useUser'
+
 function OnboardingExtension() {
+  const { user } = useUser()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!user?.hasConnectedExtension) return
+
+    navigate('/~')
+  }, [
+    user,
+    navigate,
+  ])
+
   return (
     <>
-      OnboardingExtension
+      Waiting on extension
     </>
   )
 }
