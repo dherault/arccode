1a585b018fe0b7cbaab57d0a7cd7d0728b1b3683
David Hérault <dherault@gmail.com>
2024-08-10 01:12:15 +0300

    Create first cloud functions

---
+++ arccode.dev/functions/src/index.ts
@@ -0,0 +1,115 @@
+// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
+import { logger } from 'firebase-functions'
+import { onRequest } from 'firebase-functions/v2/https'
+import { initializeApp } from 'firebase-admin/app'
+import { FieldValue, getFirestore } from 'firebase-admin/firestore'
+import { getAuth } from 'firebase-admin/auth'
+import { z } from 'zod'
+
+const app = initializeApp()
+
+const keywordRegistrySchema = z.object({}).catchall(z.object({}).catchall(z.number().nonnegative().finite()))
+
+export const registerKeywords = onRequest(
+  { cors: true },
+  async (req, res) => {
+
+    const idToken = req.headers.authorization?.split('Bearer ')[1]
+
+    if (!idToken) {
+      res.status(401).send('Unauthorized')
+
+      return
+    }
+
+    let userId: string
+
+    try {
+      const decodedIdToken = await getAuth(app).verifyIdToken(idToken)
+
+      userId = decodedIdToken.uid
+    }
+    catch (error) {
+      logger.error('Invalid token', error)
+
+      res.status(401).send('Unauthorized')
+
+      return
+    }
+
+    let keywords: Record<string, Record<string, number>>
+
+    try {
+      keywords = keywordRegistrySchema.parse(req.body.keywords)
+    }
+    catch (error) {
+      logger.error('Invalid keywords', req.body.keywords, error)
+
+      res.status(400).send('Invalid keywords')
+
+      return
+    }
+
+    const payload: Record<string, any> = {
+      updatedAt: FieldValue.serverTimestamp(),
+    }
+
+    Object.entries(keywords).forEach(([language, keywordMap]) => {
+      if (!language) return
+
+      Object.entries(keywordMap).forEach(([keyword, amount]) => {
+        if (!keyword) return
+        if (amount !== amount) return
+
+        payload[`keywords.${language}.${keyword}`] = FieldValue.increment(Math.round(amount))
+      })
+    })
+
+    await getFirestore(app)
+    .collection('users')
+    .doc(userId)
+    .update(payload)
+
+    res.status(200).send()
+  }
+)
+
+export const activateVscodeExtension = onRequest(
+  { cors: true },
+  async (req, res) => {
+    const idToken = req.headers.authorization?.split('Bearer ')[1]
+
+    if (!idToken) {
+      res.status(401).send('Unauthorized')
+
+      return
+    }
+
+    let userId: string
+
+    try {
+      const decodedIdToken = await getAuth(app).verifyIdToken(idToken)
+
+      userId = decodedIdToken.uid
+    }
+    catch (error) {
+      logger.error('Invalid token', error)
+
+      res.status(401).send('Unauthorized')
+
+      return
+    }
+
+    const payload: Record<string, any> = {
+      hasConnectedExtension: true,
+      updatedAt: FieldValue.serverTimestamp(),
+    }
+
+    await getFirestore(app)
+    .collection('users')
+    .doc(userId)
+    .update(payload)
+
+    res.status(200).send()
+  }
+)
--- packages/arccode-vscode-extension/src/constants.ts
+++ packages/arccode-vscode-extension/src/constants.ts
@@ -6,6 +6,14 @@ export const AUTHENTICATION_TYPE = 'arccode'
 
 export const AUTHENTICATION_NAME = 'Arccode'
 
+export const ACTIVATE_EXTENSION_API_URL = process.env.DEV
+  ? 'http://localhost:5001/arccode-dev/us-central/activateVscodeExtension'
+  : 'https://activatevscodeextension-ynstfmkxyq-uc.a.run.app'
+
+export const REGISTER_KEYWORDS_API_URL = process.env.DEV
+  ? 'http://localhost:5001/arccode-dev/us-central/registerKeywords'
+  : 'https://registerkeywords-ynstfmkxyq-uc.a.run.app'
+
 export const SESSIONS_SECRET_KEY = `${AUTHENTICATION_TYPE}.sessions`
 
 export const CODE_EXCHANGE_PROMISE_KEY = 'CODE_EXCHANGE_PROMISE'
