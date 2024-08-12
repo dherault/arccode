02e26e679161bafd7f2f4d96a7faa9fe8800c78d
David Hérault <dherault@gmail.com>
2024-08-12 15:55:28 +0300

    Edit administrator layout

--- arccode.dev/src/app/administrator.../layout.tsx
+++ arccode.dev/src/app/administrator.../layout.tsx
@@ -1,3 +1,16 @@
+import type { PropsWithChildren } from 'react'
+
+import AuthenticationBouncer from '~components/authentication/AuthenticationBouncer'
 import AdministratorBouncer from '~components/administrator/AdministratorBouncer'
 
-export default AdministratorBouncer
+function Layout({ children }: PropsWithChildren) {
+  return (
+    <AuthenticationBouncer>
+      <AdministratorBouncer>
+        {children}
+      </AdministratorBouncer>
+    </AuthenticationBouncer>
+  )
+}
+
+export default Layout
