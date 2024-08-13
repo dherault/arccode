d3c685011c8df1aa175d27a01687ab7c7cfe5197
David Hérault <dherault@gmail.com>
2024-08-13 10:15:50 +0300

    Publish v1.1.0

--- packages/language-keywords/src/index.ts
+++ packages/language-keywords/src/index.ts
@@ -1,19 +1,45 @@
-export { default as javascript } from './javascript'
+import javascript from './languages/javascript'
+import typescript from './languages/typescript'
+import python from './languages/python'
+import java from './languages/java'
+import c from './languages/c'
+import cpp from './languages/cpp'
+import csharp from './languages/csharp'
+import ruby from './languages/ruby'
+import php from './languages/php'
+import go from './languages/go'
 
-export { default as typescript } from './typescript'
+const keywords = {
+  c,
+  cpp,
+  csharp,
+  go,
+  java,
+  javascript,
+  php,
+  python,
+  ruby,
+  typescript,
+}
 
-export { default as python } from './python'
+export default keywords
 
-export { default as java } from './java'
+export { default as javascript } from './languages/javascript'
 
-export { default as c } from './c'
+export { default as typescript } from './languages/typescript'
 
-export { default as cpp } from './cpp'
+export { default as python } from './languages/python'
 
-export { default as csharp } from './csharp'
+export { default as java } from './languages/java'
 
-export { default as ruby } from './ruby'
+export { default as c } from './languages/c'
 
-export { default as php } from './php'
+export { default as cpp } from './languages/cpp'
 
-export { default as go } from './go'
+export { default as csharp } from './languages/csharp'
+
+export { default as ruby } from './languages/ruby'
+
+export { default as php } from './languages/php'
+
+export { default as go } from './languages/go'
