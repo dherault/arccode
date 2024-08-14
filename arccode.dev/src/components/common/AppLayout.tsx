9a54e60de8e89e6b0e58b2fe9dfd42d50f7b913f
David Hérault <dherault@gmail.com>
2024-08-14 13:15:29 +0300

    Add star on GitHub button

--- arccode.dev/src/components/common/AppLayout.tsx
+++ arccode.dev/src/components/common/AppLayout.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from 'react'
 import { Link } from 'react-router-dom'
+import GitHubButton from 'react-github-btn'
 
 import Logotype from '~components/common/logos/Logotype'
 import UserAvatarMenu from '~components/common/UserAvatarMenu'
@@ -12,7 +13,19 @@ function AppLayout({ children }: PropsWithChildren) {
           <Logotype />
         </Link>
       </div>
-      <div className="absolute top-4 right-6 z-50">
+      <div className="absolute top-4 right-6 flex items-center gap-4 z-50">
+        <div className="mt-1">
+          <GitHubButton
+            href="https://github.com/dherault/arccode"
+            data-color-scheme="no-preference: light; light: light; dark: dark;"
+            data-icon="octicon-star"
+            data-size="large"
+            data-show-count="true"
+            aria-label="Star dherault/arccode on GitHub"
+          >
+            Star
+          </GitHubButton>
+        </div>
         <UserAvatarMenu />
       </div>
       <div className="mt-[72px] grow flex flex-col">
