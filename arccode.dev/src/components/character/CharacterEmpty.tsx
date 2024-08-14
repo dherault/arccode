fe58ca8249859a8b9f5347ef03eb5278d07414b6
David Hérault <dherault@gmail.com>
2024-08-14 15:43:43 +0300

    Improve CharacterEmpty

--- arccode.dev/src/components/character/CharacterEmpty.tsx
+++ arccode.dev/src/components/character/CharacterEmpty.tsx
@@ -1,8 +1,13 @@
+import { InfoIcon } from 'lucide-react'
+
 function CharacterEmpty() {
   return (
     <div className="p-4 bg-white border rounded">
-      <div className="font-semibold">
-        Now is the time to start conding!
+      <div className="flex items-center gap-2">
+        <InfoIcon className="h-4 w-4" />
+        <div className="font-semibold">
+          Now is the time to start conding!
+        </div>
       </div>
       <div className="mt-2">
         Open VSCode and write some code to get started.
