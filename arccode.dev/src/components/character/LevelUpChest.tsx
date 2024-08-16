6b8b87c4eef2ec958b26d942ea91e002bc0c1aaf
David Hérault <dherault@gmail.com>
2024-08-16 13:51:00 +0300

    Enhance LevelUpReward

--- arccode.dev/src/components/character/LevelUpChest.tsx
+++ arccode.dev/src/components/character/LevelUpChest.tsx
@@ -4,12 +4,15 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import usePreloadImage from '~hooks/common/usePreloadImage'
 import useCharacter from '~hooks/character/useCharacter'
 
+import LevelUpReward from '~components/character/LevelUpReward'
+
 import treasureChests from '~data/treasure-chests'
 
 let animationTimeoutId: NodeJS.Timeout | null = null
 
 const SHAKE_DURATION = 500
 const SHRINK_DURATION = 150
+const POP_DURATION = 300
 
 function LevelUpChest() {
   const { levelUpsCount, levelUpsCursor, openChest } = useCharacter()
@@ -18,6 +21,7 @@ function LevelUpChest() {
   const treasureChestIndex = useMemo(() => Math.round(Math.random() * treasureChests.length), [levelUpsCursor])
   const [animation, setAnimation] = useState('shake')
   const [open, setOpen] = useState(false)
+  const [rewardDisplayed, setRewardDisplayed] = useState(false)
   const [timesClicked, setTimesClicked] = useState(0)
 
   const treasureChest = useMemo(() => treasureChests[treasureChestIndex], [treasureChestIndex])
@@ -35,6 +39,10 @@ function LevelUpChest() {
           setOpen(true)
           setAnimation('grow')
           openChest()
+
+          setTimeout(() => {
+            setRewardDisplayed(true)
+          }, SHRINK_DURATION)
         }, SHRINK_DURATION)
       }
       else {
@@ -43,7 +51,6 @@ function LevelUpChest() {
 
         animationTimeoutId = setTimeout(() => {
           setAnimation('shake')
-
         }, SHAKE_DURATION)
       }
     }
@@ -58,6 +65,7 @@ function LevelUpChest() {
     setAnimation('shake')
     setOpen(false)
     setTimesClicked(0)
+    setRewardDisplayed(false)
   }, [
     levelUpsCursor,
     levelUpsCount,
@@ -65,61 +73,87 @@ function LevelUpChest() {
 
   return (
     <div
-      className="h-[384px] w-[384px] cursor-pointer rounded-full overflow-hidden relative"
+      className="h-fit w-fit cursor-pointer rounded-full"
       onClick={handleClick}
     >
-      <motion.div
-        animate={animation}
-        initial={{
-          rotate: 0,
-          top: 0,
-          scale: 1,
-        }}
-        variants={{
-          shake: {
-            rotate: [-2, 2, 0, -2],
-            transition: {
-              duration: SHAKE_DURATION / 1000,
-              repeat: Infinity,
-            },
-          },
-          nudge: {
-            rotate: [-12, 12, 0],
-            top: [0, -32, 16, -32],
-            transition: {
-              duration: SHAKE_DURATION / 1000,
-            },
-          },
-          shrink: {
-            scale: 0,
-            transition: {
-              duration: SHRINK_DURATION / 1000,
-            },
-          },
-          grow: {
+      <div className="h-[384px] w-[384px] overflow-hidden relative">
+        <motion.div
+          animate={animation}
+          initial={{
+            rotate: 0,
+            top: 0,
             scale: 1,
-            transition: {
-              duration: SHRINK_DURATION / 1000,
+          }}
+          variants={{
+            shake: {
+              rotate: [-2, 2, 0, -2],
+              transition: {
+                duration: SHAKE_DURATION / 1000,
+                repeat: Infinity,
+              },
             },
-          },
-        }}
-        transition={{
-          duration: 0.3,
-          type: 'tween',
-        }}
-        className="relative h-full w-full"
-      >
-        <img
-          src={`/images/treasure-chests/${open ? treasureChest.open : treasureChest.closed}`}
-          alt="Treasure chest"
-          className="absolute -inset-24 min-w-[calc(100%+2*96px)]"
-        />
-      </motion.div>
-      {levelUpsCount > 1 && (
-        <div className="absolute top-24 right-24 h-8 w-8 bg-blue text-white rounded-full flex items-center justify-center">
-          {levelUpsCount}
-        </div>
-      )}
+            nudge: {
+              rotate: [-12, 12, 0],
+              top: [0, -32, 16, -32],
+              transition: {
+                duration: SHAKE_DURATION / 1000,
+              },
+            },
+            shrink: {
+              scale: 0,
+              transition: {
+                duration: SHRINK_DURATION / 1000,
+              },
+            },
+            grow: {
+              scale: 1,
+              transition: {
+                duration: SHRINK_DURATION / 1000,
+              },
+            },
+          }}
+          transition={{
+            duration: 0.3,
+            type: 'tween',
+          }}
+          className="relative h-full w-full"
+        >
+          <img
+            src={`/images/treasure-chests/${open ? treasureChest.open : treasureChest.closed}`}
+            alt="Treasure chest"
+            className="absolute -inset-24 min-w-[calc(100%+2*96px)]"
+          />
+        </motion.div>
+        {levelUpsCount > 1 && (
+          <div className="absolute top-24 right-24 h-8 w-8 bg-blue text-white rounded-full flex items-center justify-center">
+            {levelUpsCount}
+          </div>
+        )}
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <motion.div
+          animate={rewardDisplayed ? 'pop-in' : undefined}
+          initial={{
+            scale: 0,
+            x: 0,
+            y: -42,
+          }}
+          variants={{
+            'pop-in': {
+              scale: 1,
+              x: [0, -42, 0],
+              y: [0, -42, 0],
+              transition: {
+                type: 'tween',
+                duration: POP_DURATION / 1000,
+              },
+            },
+          }}
+          className="relative"
+        >
+          <LevelUpReward />
+        </motion.div>
+      </div>
     </div>
   )
 }
--- arccode.dev/src/components/character/LevelUpReward.tsx
+++ arccode.dev/src/components/character/LevelUpReward.tsx
@@ -1,18 +1,55 @@
+import { ITEM_TYPE_LABELS } from '~constants'
+
 import useCharacter from '~hooks/character/useCharacter'
 
+import { Button } from '~components/ui/Button'
+import ItemCard from '~components/character/ItemCard'
+
+import items from '~data/items'
+
 function LevelUpReward() {
-  const { levelUpsUnlockedItems } = useCharacter()
+  const { levelUpsUnlockedItems, levelUpsCount, closeChest } = useCharacter()
 
   const levelUpsUnlockedItemsEntries = Object.entries(levelUpsUnlockedItems)
 
-  if (!levelUpsUnlockedItemsEntries.length) {
-    return null
-  }
+  if (!levelUpsUnlockedItemsEntries.length) return null
 
   return (
-    <>
-      LevelUpReward
-    </>
+    <div className="p-4 bg-white border rounded select-none max-h-[320px] overflow-auto">
+      <div className="text-center">
+        You unlocked:
+      </div>
+      <div
+        className="mt-4 mx-auto w-fit grid gap-y-2 gap-x-4"
+        style={{ gridTemplateColumns: `repeat(${Math.min(4, levelUpsUnlockedItemsEntries.length)}, 1fr)` }}
+      >
+        {levelUpsUnlockedItemsEntries.map(([itemId, amount]) => (
+          <div
+            key={itemId}
+            className="relative w-[88px] aspect-square flex flex-col items-center text-center"
+          >
+            <div className="mb-1 text-xs font-light text-neutral-500">
+              {ITEM_TYPE_LABELS[items[itemId].type]}
+            </div>
+            <ItemCard item={items[itemId]} />
+            <div className="mt-1 text-xs text-neutral-700 leading-none">
+              {items[itemId].name}
+            </div>
+            {amount > 1 && (
+              <div className="absolute top-2 -right-3 h-6 w-6 bg-neutral-50 border rounded-full text-xs text-neutral-500 flex items-center justify-center">
+                x
+                {amount}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="mt-4 flex justify-center">
+        <Button onClick={closeChest}>
+          {levelUpsCount > 0 ? 'Open next box' : 'Close'}
+        </Button>
+      </div>
+    </div>
   )
 }
 
