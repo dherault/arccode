093953bec7d30348ee9e2e2d839502237f825f31
David Hérault <dherault@gmail.com>
2024-08-17 21:49:43 +0300

    Add quote to LandingQuote

--- arccode.dev/src/components/landing/LandingQuote.tsx
+++ arccode.dev/src/components/landing/LandingQuote.tsx
@@ -1,3 +1,5 @@
+import Balancer from 'react-wrap-balancer'
+
 import Logo from '~components/common/logos/Logo'
 
 function LandingQuote() {
@@ -16,10 +18,12 @@ function LandingQuote() {
           style={{ filter: 'hue-rotate(333deg)' }}
         />
         <div className="absolute inset-0 flex items-center">
-          <div className="-mt-6 px-16 text-white">
+          <div className="-mt-4 px-16 text-white">
             <Logo className="h-12" />
-            <div className="mt-6 text-xl w-2/3 font-semibold">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rhoncus sed justo eget feugiat. Aliquam pellentesque metus vel tellus porttitor tempus."
+            <div className="mt-6 text-xl font-semibold">
+              <Balancer>
+                "Coding is inherently fun — it's an act of creation that yields visible results. However, it can also become repetitive and laborious. That's why I created Arccode: to bring joy to your day and shield you from monotony. By incorporating variable rewards and a sense of progress into coding, I believe we can make the experience even more enjoyable. So, have fun coding!"
+              </Balancer>
             </div>
             <div className="mt-6 font-semibold">
               David Hérault
