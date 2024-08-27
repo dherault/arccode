b2c1c2b97f73c48cd4e5eeca302f3c07ee42d5ec
David Hérault <dherault@gmail.com>
2024-08-27 20:09:13 +0300

    Fix recap email function

--- arccode.dev/functions/src/logic/sendDailyRecapEmails.ts
+++ arccode.dev/functions/src/logic/sendDailyRecapEmails.ts
@@ -18,37 +18,53 @@ const RECAP_HOUR = IS_DEV
 async function sendDailyRecapEmails() {
   const timezoneOffset = getTimezoneOffsetAtHour(RECAP_HOUR)
 
-  if (timezoneOffset === null) return 0
+  if (timezoneOffset === null) return
 
   const yesterday = DateTime.now().minus({ days: 1 }).toISO()
-  const aBitBeforeYesterday = DateTime.now().minus({ days: 1, minutes: 5 }).toISO()
+  const aBitAfterYesterday = DateTime.now().minus({ days: 1 }).plus({ minutes: 15 }).toISO()
 
   const users = await firestore.collection('users')
       .where('hasConnectedExtension', '==', true)
       .where('updatedAt', '>=', yesterday)
-      .where('sentDailyRecapEmailAt', '<=', aBitBeforeYesterday)
+      .where('sentDailyRecapEmailAt', '<=', aBitAfterYesterday)
       .where('timezoneOffset', '==', timezoneOffset)
       .get()
 
   let count = 0
 
-  // console.log(users.docs.length, 'users')
+  logger.log(`Considering ${users.docs.length} users`)
 
   for (const userDoc of users.docs) {
     const user = userDoc.data() as User
 
     if (!user.email) continue
 
+    logger.log(`Considering user ${user.email}`)
+
     const keywordRegistry = filterKeywordRegistry(diffKeywordRegistries(user.character.keywordRegistry, user.character.lastDailyRecapKeywordRegistry))
 
-    if (!Object.keys(keywordRegistry).length) continue
-    if (!Object.keys(user.character.levelUpKeywordRegistry).length) continue
+    if (!Object.keys(keywordRegistry).length) {
+      logger.log('No keywordRegistry')
+
+      continue
+    }
+    if (!Object.keys(user.character.levelUpKeywordRegistry).length) {
+      logger.log('No levelUpKeywordRegistry')
+
+      continue
+    }
 
     const levelUpCount = sumKeywordRegistry(user.character.levelUpKeywordRegistry)
 
-    if (!levelUpCount) continue
+    if (!levelUpCount) {
+      logger.log('No levelUpCount')
+
+      continue
+    }
 
     try {
+      logger.log('Sending recap email')
+
       await sendRecapEmail({
         email: IS_DEV ? 'delivered@resend.dev' : user.email,
         name: user.character.name,
@@ -77,7 +93,7 @@ async function sendDailyRecapEmails() {
     }
   }
 
-  return count
+  logger.log(`Sent ${count} daily recap emails`)
 }
 
 export default sendDailyRecapEmails
--- arccode.dev/functions/src/routes/sendEmails.ts
+++ arccode.dev/functions/src/routes/sendEmails.ts
@@ -1,6 +1,5 @@
 import { HttpsError, onCall } from 'firebase-functions/v2/https'
 import { onSchedule } from 'firebase-functions/v2/scheduler'
-import { logger } from 'firebase-functions/v2'
 
 import { getUserFromCallableRequest } from '../authentication/getUser'
 import sendDailyRecapEmails from '../logic/sendDailyRecapEmails'
@@ -8,9 +7,7 @@ import sendDailyRecapEmails from '../logic/sendDailyRecapEmails'
 export const sendRecapEmailsCron = onSchedule(
   { secrets: ['RESEND_API_KEY'], schedule: '0 * * * *' }, // Every hour at minute 0
   async () => {
-    const count = await sendDailyRecapEmails()
-
-    logger.log(`Sent ${count} recap emails`)
+    await sendDailyRecapEmails()
   }
 )
 
@@ -22,10 +19,8 @@ export const sendRecapEmailsRequest = onCall(
     if (!administratorUser) throw new HttpsError('permission-denied', 'You are not authenticated')
     if (!administratorUser.isAdministrator) throw new HttpsError('permission-denied', 'You are not an administrator')
 
-    const count = await sendDailyRecapEmails()
+    await sendDailyRecapEmails()
 
-    logger.log(`Sent  ${count} recap emails`)
-
-    return { count }
+    return { message: 'ok' }
   }
 )
